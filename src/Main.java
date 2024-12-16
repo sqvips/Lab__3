0591fd4689eb0016a7ba9fa8f591e1e48947e257
sqvips <rockaturbo@gmail.com>
2024-12-17 00:13:46 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
-// Главный класс для запуска программы
 public class Main {
     public static void main(String[] args) {
+
         Graph graph = new Graph(); // Создаем новый граф
 
         // Добавляем рёбра в граф
@@ -9,35 +9,51 @@ public class Main {
         graph.addEdge(1, 3, 15);
         graph.addEdge(2, 1, 8);
         graph.addEdge(4, 5, 19);
+        graph.addEdge(3, 4, 12);
+        graph.addEdge(5, 1, 6);
+        graph.addEdge(2, 4, 11);
+        graph.addEdge(3, 5, 9);
+        graph.addEdge(1, 4, 14);
+        graph.addEdge(2, 5, 20);
+        graph.addEdge(4, 6, 30);
+        graph.addEdge(6, 7, 25);
+        graph.addEdge(7, 1, 5);
 
         Graph graph1 = new Graph(); // Создаем еще один граф
-        graph1.addEdge(1, 2, 10);
-        graph1.addEdge(2, 3, 20);
+        graph1.addEdge(5, 1, 6);
+        graph1.addEdge(100, 65, 1);
+        // Создаем объект для отображения объединенного графа
+        GraphDisplay graphDisplay = new GraphDisplay(graph);
 
-        graph.combine(graph1); // Объединяем два графа
-        GraphDisplay graphDisplay = new GraphDisplay(graph); // Создаем объект для отображения первого графа
-        GraphDisplay graphDisplay1 = new GraphDisplay(graph1); // Создаем объект для отображения второго графа
-        graphDisplay.show(); // Отображаем первый граф
+        graphDisplay.show();
 
-        graph.removeTopsMinMax(); // Удаляем вершины с максимальным весом и минимальным количеством входящих рёбер
-        graphDisplay.show(); // Отображаем граф после удаления
+        graphDisplay.showTop();
 
-        graphDisplay.showTop(); // Отображаем все вершины графа
-        graphDisplay.showTop2Limit(18); // Отображаем вершины с весом входящих рёбер <= 18
-        graphDisplay.showTopsSortedWeight(); // Отображаем вершины, отсортированные по весу
+        graphDisplay.showTop2Limit(30);
 
-        graph.combineEdges2(2, 1); // Объединяем рёбра между вершинами 2 и 1
-        graphDisplay.show(); // Отображаем граф после объединения
+        graphDisplay.showTopsSortedWeight();
 
-        graph.changeEdgeWeight(1, 2, -100); // Изменяем вес ребра между вершинами 1 и 2 на -100
-        graphDisplay.show(); // Отображаем граф после изменения веса
+        graph.combineEdges2(1, 2);
+        graphDisplay.show();
 
-        graph.removeEdgesBetween(1, 2); // Удаляем рёбра между вершинами 1 и 2
-        graphDisplay.show(); // Отображаем граф после удаления рёбер
+        graph.combineEdges(1, 2);
+        graphDisplay.show();
 
-        graph1.removeTops(2); // Удаляем вершину 2 из второго графа
-        graphDisplay.show(); // Отображаем первый граф после удаления
-        graphDisplay.showTop(); // Отображаем все вершины первого графа
+        graph.changeEdgeWeight(1, 2, 5);
+        graphDisplay.show();
+
+        graph.removeEdgesBetween(1, 2);
+        graphDisplay.show();
+
+        graph.removeTops(3);
+        graphDisplay.show();
+
+        graph.removeTopsMinMax();
+        graphDisplay.show();
+
+        graph.combine(graph1);
+        graphDisplay.show();
+
     }
 }
 
